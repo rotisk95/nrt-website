66edde1e2daaf4d9066f1d9e42c448a84c4e3b24
Dragoncharlie <tihaya187@mail.ru>
2017-09-26 10:04:36 +0700

    add modalShow func

--- documentation/assets/js/menu.js
+++ documentation/assets/js/menu.js
@@ -6,23 +6,3 @@ $(function(){
         return false;
     });
 });
-
-
-
-
-// function fixedHeader() {
-//     var ww = $(window).scrollTop();
-//     if(ww > 0){
-//         $('.menu').addClass('menu--active');
-//         $('.mobile-menu').addClass('mobile-menu--active');
-//     }else{
-//         $('.menu').removeClass('menu--active');
-//         $('.mobile-menu').removeClass('mobile-menu--active');
-//     }
-// }
-// fixedHeader();
-// $(window).on('scroll', function () {
-//     fixedHeader();
-// });
-
-
